8f6f974c571bf70a6c7fa023a9a10e6699976257
Robert Castelo <robert.castelo@upf.edu>
2022-10-10 18:55:55 +0200

    Replace identation spaces by tabs

--- src/friends.py
+++ src/friends.py
@@ -1,19 +1,20 @@
 ## DO NOT REMOVE THIS LINE
 def main() :
-  ## DO NOT REMOVE THESE TWO LINES
-  x = int(input("Enter a positive integer number: "))
-  y = int(input("Enter another positive integer number: "))
+	# DO NOT REMOVE THESE TWO LINES
+	x = int(input("Enter a positive integer number: "))
+	y = int(input("Enter another positive integer number: "))
 
-  ## YOUR CODE SHOULD START HERE AT THE SAME
-  ## INDENTATION AS THIS COMMENT
-  
-  
-  ## THE LAST LINES OF YOUR CODE SHOULD EITHER
-  ## PRINT THE SENTENCE "friends" or "no friends"
-  ## REPLACE THE FOLLOWING LINE BY WHATEVER LINES
-  ## OF CODE END UP WRITING "friends" or "no friends"
-  print("no friends")
+	## YOUR CODE SHOULD START HERE AT THE SAME
+	## INDENTATION AS THIS COMMENT
+
+
+
+	## THE LAST LINES OF YOUR CODE SHOULD EITHER
+	## PRINT THE SENTENCE "friends" or "not friends"
+	## REPLACE THE FOLLOWING LINE BY WHATEVER LINES
+	## OF CODE END UP WRITING "friends" or "not friends"
+	print("not friends")
 
 ## DO NOT REMOVE THESE LINES
 if __name__ == "__main__" :
-  main()
+	main()
